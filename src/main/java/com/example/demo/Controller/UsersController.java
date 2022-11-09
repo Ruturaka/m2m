15a58f3accf2c5cbfca858d1d204d4b61aaba54c
Ruturaka2700 <ruturaka2700@gmail.com>
2022-11-09 18:39:19 +0530

    initial commit

---
+++ src/main/java/com/example/demo/Controller/UsersController.java
@@ -0,0 +1,15 @@
+package com.example.demo.Controller;
+import com.example.demo.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class UsersController {
+    @Autowired
+    private UsersService usersService;
+    @RequestMapping("/getUsers")
+    public ResponseEntity<Object> getAllUsers() {
+        return usersService.getAllUsers();
+    }
+}
